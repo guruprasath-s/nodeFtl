8148046c442dc52bc0b7d54a20e330da60cae42b
guru <mailtosguru@gmail.com>
2017-12-16 19:43:15 +0530

    Commit 2

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 var express = require("express");
 var http = require("http");
 var fs = require("fs");
-//var request = require("request");
 var ftl = require("node-ftl");
+var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use('/', function(req, res){
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.use('/home', function(req, res){
 
 	res.sendFile(__dirname + '/index.html');
 
 });
 
-
 var router = express.Router();
-router.get("/ftl", function(req, res) {
-	var template = req.body.template;
-    var dataModel = req.body.dataModel;
+router.post("/ftl", function(req, res) {
+    var template = req.body.template;
+    var dataModel = req.body.dataModel;    
 	ftl.processTemplate({
         data: dataModel,
         settings: {
             encoding: 'utf-8',
-            viewFolder: path.join(process.cwd(), 'nodeFtl/')
+            viewFolder: path.join(process.cwd(), '/')
         },
         filename: 'demo.ftl'
     }).on('end', function(err, html) {
         res.send(html);
+        /*res.send({
+            "code": 200,
+            "success": "success",
+            "data": html
+        });*/
     });
+    //res.send(stringify(req));
 });
 app.use('/api', router);
+
 app.listen(8080);
